8b020547a9cdecf6c2cbfc82595550e42b343b7f
Daniel Rothmaler <daniel.rothmaler@nubix.biz>
2015-06-17 00:00:56 +0200

    fixed git reference

--- willgit.rb
+++ willgit.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Willgit < Formula
   homepage 'http://gitorious.org/willgit/mainline/'
-  head 'git://gitorious.org/willgit/mainline.git'
+  head 'https://gitorious.org/willgit/mainline.git'
 
   def install
     prefix.install 'bin'
